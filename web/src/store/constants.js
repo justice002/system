6efdc08a4167158bc903727be76458e6daf0657f
LEIQIHAO <130330693+LEIQIHAO@users.noreply.github.com>
2024-11-17 17:34:40 +0800

    Revert "Server branch"

---
+++ web/src/store/constants.js
@@ -0,0 +1,13 @@
+
+const BASE_URL = 'http://121.37.219.159:13001';
+// const http://121.37.219.159:13001 = 'http://124.221.83.178:9015'
+
+const USER_ID = 'user_id';
+const USER_NAME = 'user_name';
+const USER_TOKEN = 'user_token';
+
+const ADMIN_USER_ID = 'admin_user_id';
+const ADMIN_USER_NAME = 'admin_user_name';
+const ADMIN_USER_TOKEN = 'admin_user_token';
+
+export { BASE_URL, USER_TOKEN, USER_NAME, USER_ID, ADMIN_USER_ID, ADMIN_USER_NAME, ADMIN_USER_TOKEN };
